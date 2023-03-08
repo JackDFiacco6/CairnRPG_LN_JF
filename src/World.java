c5d524111247671f2957a671dee9824cef00a1cf
JackDFiacco6 <123217007+JackDFiacco6@users.noreply.github.com>
2023-03-08 17:09:10 -0500

    Revert "Added code to World Class"
    This reverts commit f4575b4c487e8b1b33481680fe8d32d2efec3314.

--- src/World.java
+++ src/World.java
@@ -3,29 +3,4 @@ package src;
 public class World {
     private String gameName;
     private String publisher;
-
-    World () {
-        gameName = "Fjords of Capybaras";
-        String publisher = "";
-    }
-    public World (String gameName, String publisher) {
-        this.gameName = gameName;
-        this.publisher = publisher;
-    }
-    // Getters + Setters
-    public String getGameName() {
-        return gameName;
-    }
-
-    public void setGameName(String gameName) {
-        this.gameName = gameName;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
 }
