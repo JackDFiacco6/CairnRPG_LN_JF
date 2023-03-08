c3b39c1e49678a244f7daf83d6a1073a96250afc
JackDFiacco6 <123217007+JackDFiacco6@users.noreply.github.com>
2023-03-08 18:19:22 -0500

    Added getters and setters for World class

--- src/World.java
+++ src/World.java
@@ -12,4 +12,21 @@ public class World {
         this.gameName = gameName;
         this.publisher = publisher;
     }
+    // Getters + Setters
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+}
 }
