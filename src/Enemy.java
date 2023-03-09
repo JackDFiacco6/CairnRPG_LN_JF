a0bf846cb1f4d37d9e189b1b5962cdae95baee3b
Laina Nguyen <78044530+lainanguyen@users.noreply.github.com>
2023-03-08 21:15:01 -0500

    add fight method to enemy class

--- src/Enemy.java
+++ src/Enemy.java
@@ -4,13 +4,23 @@ public class Enemy extends Character {
     private int magicPoints;
     private int magicPower = 30;
 
+    public Enemy(double health, double attackPower, boolean isAlive) {
+        super("Evil Capy", health, attackPower, isAlive);
+    }
     public Enemy(String name, double health, double attackPower, boolean isAlive) {
         super(name, health, attackPower, isAlive);
     }
 
-    public Enemy(double health, double attackPower, boolean isAlive) {
-        super("Evil Capy", health, attackPower, isAlive);
-    }
 
+
+    // Fight
+    public boolean fight(Hero hero){
+        double stamina = Math.random();
+        if (randomValue < 0.5 || magicPoints >= 30){
+            attackPower = this.getAttackPower() * 4;
+        } else {
+            attackPower = this.getAttackPower();
+        }
+    }
 
 }
