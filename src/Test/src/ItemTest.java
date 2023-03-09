115bbf9dd8387a644eb839a7d0a81fdb9473a498
Laina Nguyen <78044530+lainanguyen@users.noreply.github.com>
2023-03-09 12:54:35 -0500

    testing character methods

--- src/Test/src/ItemTest.java
+++ src/Test/src/ItemTest.java
@@ -7,22 +7,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class ItemTest {
 
     @Test
-    void getName() {
-    }
-
-    @Test
-    void setName() {
-    }
-
-    @Test
-    void getHealingPower() {
-    }
-
-    @Test
-    void setHealingPower() {
-    }
-
-    @Test
     void info() {
     }
 }
