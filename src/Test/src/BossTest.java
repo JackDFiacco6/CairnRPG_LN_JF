1b083982992fa14032d06e61295fd9e915e96ccb
JackDFiacco6 <123217007+JackDFiacco6@users.noreply.github.com>
2023-03-09 13:52:39 -0500

    Added Boss unit test

--- src/Test/src/BossTest.java
+++ src/Test/src/BossTest.java
@@ -4,21 +4,24 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BossTest {
+public class BossTest {
 
     @Test
-    void fight() {
+    public void testConstructor() {
+        Boss boss = new Boss(100, 10, true);
+        assertEquals("Capy Boss", boss.getName(), "Boss's name should be Capy Boss");
+        assertEquals(100, boss.getHealth(), "Boss's health should be 100");
+        assertEquals(10, boss.getAttackPower(), "Boss's attack power should be 10");
+        assertTrue(boss.isAlive(), "Boss should be alive");
+        assertEquals(3, boss.getUltrasLeft(), "Initial ultrasLeft should be 3");
     }
 
     @Test
-    void getUltrasLeft() {
+    public void testFight() {
+        Boss boss = new Boss(100, 10, true);
+        Hero hero = new Hero(100, 5, true);
+        boss.fight(hero);
+        assertTrue(hero.getHealth() < 100, "Hero's health should be less than 100 after the fight");
     }
 
-    @Test
-    void setUltrasLeft() {
-    }
-
-    @Test
-    void info() {
-    }
 }
