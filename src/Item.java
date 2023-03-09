3c0e0f1e66de9ac36bc657054d9bb4139444b524
JackDFiacco6 <123217007+JackDFiacco6@users.noreply.github.com>
2023-03-09 08:35:39 -0500

    Updated Item class

--- src/Item.java
+++ src/Item.java
@@ -3,4 +3,9 @@ package src;
 public class Item {
     private String name;
     private int healingPower;
+
+    Item () {
+        name = "Flask of crismon tears";
+        healingPower = 25;
+    }
 }
