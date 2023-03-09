44fa273aea1556b22d156d2060b9b1337535e718
Laina Nguyen <78044530+lainanguyen@users.noreply.github.com>
2023-03-09 13:09:22 -0500

    test constructors and methods for item class

--- src/Test/src/ItemTest.java
+++ src/Test/src/ItemTest.java
@@ -4,9 +4,25 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ItemTest {
+public class ItemTest {
+    @Test
+    public void testConstructor1() {
+        Item item = new Item();
+        assertEquals("Poison", item.getName());
+        assertEquals(25, item.getHealingPower());
+    }
 
     @Test
-    void info() {
+    public void testConstructor2() {
+        Item item = new Item("Potion", 50);
+        assertEquals("Potion", item.getName());
+        assertEquals(50, item.getHealingPower());
     }
+
+    @Test
+    public void testItemInfo() {
+        Item item = new Item("Potion", 50);
+        assertEquals("Item name: Potion, Healing Power: 50", item.Info());
+    }
+
 }
