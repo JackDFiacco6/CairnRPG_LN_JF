2c328c1a54bb531af1abc9e17d448290480aede5
Laina Nguyen <78044530+lainanguyen@users.noreply.github.com>
2023-03-09 09:08:23 -0500

    update fight function for hero class

--- src/Hero.java
+++ src/Hero.java
@@ -6,15 +6,45 @@ public class Hero extends Character {
     private float money = 0.0f;
     private Item[] inventory = new Item[5];
 
-    // Constructors
-    public Hero(double health, double attackPower, boolean isAlive){
-        super(health, attackPower, isAlive);
-        // Then add one Potion to the inventory (i.e. newItem('Potion',20))
+    // Methods
+    public void fight(Enemy evil){
+        double stamina = Math.random();
+        double damage = attackPower;
+        if (stamina < 0.5) {
+            attackPower = this.getAttackPower() * 2;
+        } else if (stamina < 0.9) {
+            attackPower = this.getAttackPower() * 3;
+        } else {
+            attackPower = this.getAttackPower();
+        }
+        System.out.println("Damage caused: " + damage + " hp.");
     }
 
-    public boolean fight(Enemy evil){
-        // Decide between using a basic attack that uses attackPower or special attack
-        // Then print to console how much damage
+    public int useItem(int i){
+        // use first item by adding to your health the amount of healing power it has
+        //print to the console how much you were healed, the character name, and name of item
+        // ex: A 'potion' healed Johnny by 15 hp
+        System.out.println("A " + i + " healed " + name + " by" + Item.healingPower + " hp.");
+    }
+
+    public void levelUp(){
+        if (this.experience > 20) {
+            this.level += 1;
+            System.out.println("Level up");
+        }
+    }
+
+    public boolean addToInventory(Item item) {
+        // Add to inventory
+    }
+
+    public String showInventory() {
+        // Show inventory
+    }
+
+    // Display hero's info
+    public String info() {
+        return "Name: " + this.name + "\nHealth: " + this.health + "\nAttack Power:" + attackPower + "\nIs Alive" + isAlive + "Money: " + this.money + "\nExperience: " + this.experience + "\nLevel:" + level + "\ninventory:" + inventory.toString();
     }
 
     // Getters and setters
@@ -50,14 +80,11 @@ public class Hero extends Character {
         this.inventory = inventory;
     }
 
-    public int useItem(int item){
-        System.out.println("Used" + item);
-        return item;
+    // Constructors
+    public Hero(double health, double attackPower, boolean isAlive){
+        super(health, attackPower, isAlive);
+        // Then add one Potion to the inventory (i.e. newItem('Potion',20))
     }
 
-    // Methods
-    public boolean levelUp(){
-
-    }
 
 }
