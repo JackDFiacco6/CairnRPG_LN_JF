1cf83e80c73fbb54fb0e2ab447576842fb08aa3b
JackDFiacco6 <123217007+JackDFiacco6@users.noreply.github.com>
2023-03-09 11:48:25 -0500

    Created unit test for Enemy class

---
+++ src/Test/src/EnemyTest.java
@@ -0,0 +1,20 @@
+package src;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemyTest {
+
+    @Test
+    void fight() {
+    }
+
+    @Test
+    void getMagicPoints() {
+    }
+
+    @Test
+    void setMagicPoints() {
+    }
+}
