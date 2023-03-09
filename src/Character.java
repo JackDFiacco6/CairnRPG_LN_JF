5386e162a080818f3d237f70e935e06fea0c0e5a
JackDFiacco6 <123217007+JackDFiacco6@users.noreply.github.com>
2023-03-08 23:35:53 -0500

    Added getters and setters for Character class

--- src/Character.java
+++ src/Character.java
@@ -14,11 +14,45 @@ public class Character {
         attackPower = 80;
         isAlive = true;
     }
+
     //Constructor
     public Character(String name, double health, double attackPower, boolean isAlive) {
         this.name = name;
         this.healingPower = health;
         this.attackPower = attackPower;
         this.isAlive = isAlive;
+    }
+
+    //getter and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHealth() {
+        return healingPower;
+    }
+
+    public void setHealth(double health) {
+        this.healingPower = health;
+    }
+
+    public double getAttackPower() {
+        return attackPower;
+    }
+
+    public void setAttackPower(float attackPower) {
+        this.attackPower = attackPower;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
     }
 }
