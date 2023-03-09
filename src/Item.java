15e83558a488023257de61272cce259dfb01d8c7
JackDFiacco6 <123217007+JackDFiacco6@users.noreply.github.com>
2023-03-09 08:57:02 -0500

    Added constructor in Item class

--- src/Item.java
+++ src/Item.java
@@ -5,7 +5,12 @@ public class Item {
     private int healingPower;
 
     Item () {
-        name = "Flask of crismon tears";
+        name = "Flask of crimson tears";
         healingPower = 25;
+    }
+    //Constructor
+    public Item(String name, int healingPower) {
+        this.name = name;
+        this.healingPower = healingPower;
     }
 }
