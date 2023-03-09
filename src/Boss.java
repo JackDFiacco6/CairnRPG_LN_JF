a5243e75022e25ae23cd83bf8728869da1db760e
Laina Nguyen <78044530+lainanguyen@users.noreply.github.com>
2023-03-08 21:23:15 -0500

    add constructor to boss class

--- src/Boss.java
+++ src/Boss.java
@@ -2,4 +2,8 @@ package src;
 
 public class Boss {
     private int ultrasLeft;
+    public Boss (double health, double attackPower, boolean isAlive) {
+        super("Capy Boss", health, attackPower, isAlive);
+        this.ultrasLeft = 3;
+    }
 }
