07af3c76ecb88c62a30a6e2304b5ab1c51d2b265
JackDFiacco6 <123217007+JackDFiacco6@users.noreply.github.com>
2023-03-08 20:53:33 -0500

    Updated character class and added constructor

--- src/Character.java
+++ src/Character.java
@@ -3,8 +3,22 @@ package src;
 import com.sun.jdi.PathSearchingVirtualMachine;
 
 public class Character {
-    private String name;
-    private float health;
-    private float healingPower;
-    private boolean isAlive;
+    public String name;
+    public double attackPower;
+    public double healingPower;
+    public boolean isAlive;
+
+    Character() {
+        name = "Capi";
+        healingPower = 100;
+        attackPower = 80;
+        isAlive = true;
+    }
+    //Constructor
+    public Character(String name, double health, double attackPower, boolean isAlive) {
+        this.name = name;
+        this.healingPower = health;
+        this.attackPower = attackPower;
+        this.isAlive = isAlive;
+    }
 }
